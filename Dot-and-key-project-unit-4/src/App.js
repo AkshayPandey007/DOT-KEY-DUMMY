2e9078d18aa5cce641d98c8766a1b37b9b34ce5a
AkshayPandey007 <101665005+AkshayPandey007@users.noreply.github.com>
2022-06-16 22:56:07 +0530

    akay

---
+++ Dot-and-key-project-unit-4/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Form from './CheckoutComponent/Form';
+import {Allroutes} from "./AkshayComponent/Allroutes"
+// import Footer from './HomePageComponents/Footer';
+// import Moisturizer from './HomePageComponents/Moisturizer';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar/> */}
+      <Allroutes/>
+      {/* <Moisturizer/> */}
+      {/* <Footer/> */}
+     {/* <Form/> */}
+    </div>
+  );
+}
+
+export default App;
